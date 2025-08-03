05b101c8390769c71e7d64492bf597fd4b0eceef
Eduard Yevchenko <82526715+Exlizardium@users.noreply.github.com>
2025-08-03 18:29:49 +0000

    Publish to GitHub Pages

---
+++ js/index.js
@@ -0,0 +1,16 @@
+function init(){
+    import('./switch.js');
+    import('./popup.js');
+    import('./encode.js');
+    import('./decode.js');
+}
+
+const totalPartials = document.querySelectorAll(
+  '[hx-trigger="load"], [data-hx-trigger="load"]'
+).length;
+let loadedPartialsCount = 0;
+
+document.body.addEventListener("htmx:afterOnLoad", () => {
+  loadedPartialsCount++;
+  if (loadedPartialsCount === totalPartials) init();
+});
